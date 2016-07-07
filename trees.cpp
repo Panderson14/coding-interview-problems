4db861a9ab09eb42ff87a07742b0c60ec202551a
Patrick Anderson <psa5dg@virginia.edu>
2016-07-06 20:19:04 -0400

    trees 07-06-2016

--- trees.cpp
+++ trees.cpp
@@ -23,3 +23,14 @@ TreeNode* createTree(int[] arr, int lowInd, int highInd) {
 	return mid;
 }
 
+/*************************
+ * “Given the value of 2 integers in a BST, find nearest common ancestor. Can assume both exist. Return the ancestor.” (PIE, 07-06-2016)
+*************************/
+
+TreeNode* ancestor(int x, int y, TreeNode* root) {
+	if (x == root->value) return root;
+	if (y == root->value) return root;
+	if (x < root->value && y < root->value) return ancestor(x, y, root->left);
+	if (x > root->value && y > root->value) return ancestor(x, y, root->right);
+	return root;
+}
